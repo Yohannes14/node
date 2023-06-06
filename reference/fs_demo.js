5a73908aca20da8b1474ece9fd6b329629018667
Yohannes <ydegu@ADMIN.CROSSROADS.CO>
2023-06-06 15:24:32 +0300

    write and rename file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -11,25 +11,46 @@ const path = require("path");
 // });
 
 // Create and write to file
-fs.writeFile(
+// fs.writeFile(
+//   path.join(__dirname, "/test", "hello.txt"),
+//   "Hello World",
+//   (err) => {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log("File written to ...");
+
+//     ///append file
+//     fs.appendFile(
+//       path.join(__dirname, "/test", "hello.txt"),
+//       "I Love Node",
+//       (err) => {
+//         if (err) {
+//           throw err;
+//         }
+//         console.log("File written to ...");
+//       }
+//     );
+//   }
+// );
+
+// Read file
+
+// fs.readFile(path.join(__dirname, "/test", 'hello.txt'), 'utf8', (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(data);
+// });
+
+// Rename file
+fs.rename(
   path.join(__dirname, "/test", "hello.txt"),
-  "Hello World",
+  path.join(__dirname, "/test", "helloworld.txt"),
   (err) => {
     if (err) {
       throw err;
     }
-    console.log("File written to ...");
-
-    ///append file
-    fs.appendFile(
-      path.join(__dirname, "/test", "hello.txt"),
-      "I Love Node",
-      (err) => {
-        if (err) {
-          throw err;
-        }
-        console.log("File written to ...");
-      }
-    );
+    console.log("File renamed....");
   }
 );
