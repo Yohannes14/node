7a2c82a70f6ba090019fca026191f2d83430c1f9
Yohannes <ydegu@ADMIN.CROSSROADS.CO>
2023-06-06 15:31:37 +0300

    os informatin

---
+++ reference/os_demo.js
@@ -0,0 +1,24 @@
+const os =require('os');
+
+//platform
+console.log(os.platform())
+
+// cpu Arch 
+console.log(os.arch())
+
+//CPU Core Info
+console.log(os.cpus())
+
+// Free memory
+console.log(os.freemem())
+
+// Total memory
+console.log(os.totalmem())
+
+
+/// Home dir
+console.log(os.homedir())
+
+
+/// updtime
+console.log(os.uptime())
